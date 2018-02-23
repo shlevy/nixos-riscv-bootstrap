000c070efd2d55b17f7dd0ffd1923d414b90a4b8
Shea Levy <shea@shealevy.com>
2018-02-23 12:32:07 -0500

    Mount a nix store with Nix, busybox, and all kernel modules.

--- README.md
+++ README.md
@@ -4,12 +4,28 @@ nixos-riscv-bootstrap
 Nix expressions to help bootstrap our way to full RISC-V support on
 NixOS.
 
+Requirements
+------------
+The scripts in this project require that you have [Nix] installed and
+a recent [nixpkgs] checkout available in your [$NIX_PATH].
+
+Initializing the base Nix store image
+---------------------------------------
+The VM needs an image to mount as the Nix store, which uses as its
+base an image containing core packages such as Nix itself and busybox.
+The `make-image` script can be used to create an image on top of the
+base image, but because images cannot be rebased you must re-create
+the image if you want to pick up changes to the core package.
+
+If you run the VM with no image, it will create one for you.
+
 Running the VM
 ----------------
 
-`run-vm` will build and run the VM described in [default.nix]. It
-requires that you have [Nix] installed and a recent [nixpkgs] checkout
-available in your [$NIX_PATH].
+`run-vm` will build and run the VM described in [default.nix]. You
+will be dropped into a busybox shell with the core packages available.
+You can specify extra flags to the qemu-system command line as
+arguments to `run-vm`
 
 [default.nix]: ./default.nix
 [Nix]: https://nixos.org/nix
--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
 { nixpkgs ? <nixpkgs> # TODO peg revision once stable
+, imageSize ? "2G"
 }:
 let lib = import (nixpkgs + "/lib");
     crossSystem = lib.systems.examples.riscv64;
     crossPkgs = import nixpkgs { inherit crossSystem; };
     pkgs = import nixpkgs {};
+    closureInfo = pkgs.callPackage ./closure-info.nix {};
     busybox =
       lib.overrideDerivation
         (crossPkgs.busybox.override { enableStatic = true; })
@@ -22,7 +24,9 @@ let lib = import (nixpkgs + "/lib");
       initrd-modules = pkgs.makeModulesClosure
         { inherit kernel;
           firmware = null;
-          rootModules = [ "virtio_scsi" "btrfs" "crc32c" "sd_mod" "virtio_mmio" ];
+          rootModules = [ "virtio_scsi" "virtio_mmio" "sd_mod"
+                          "btrfs" "crc32c"
+                        ];
         };
       init = pkgs.writeScriptBin "init" ''
         #!${busybox}/bin/sh
@@ -34,6 +38,24 @@ let lib = import (nixpkgs + "/lib");
         modprobe virtio_scsi
         modprobe crc32c
         modprobe btrfs
+
+        profile="$(grep -o 'profile=[^ ]*' /proc/cmdline | \
+          sed 's/^profile=//')"
+        export PATH="$profile/bin":$PATH
+
+        mkdir /nix-cleanup
+        mount --bind /nix /nix-cleanup
+        mount /dev/sda1 /nix
+        rm -fR /nix-cleanup/*
+        umount /nix-cleanup
+        rmdir /nix-cleanup
+
+        unlink /bin
+        ln -sf "$profile"/bin /
+        unlink /lib/modules
+        rmdir /lib
+        ln -sf "$profile"/lib /lib
+        unlink /init
         exec -a init ash
       '';
       initrd = pkgs.makeInitrd
@@ -49,19 +71,70 @@ let lib = import (nixpkgs + "/lib");
               }
             ];
         };
-      bbl = crossPkgs.riscv-pk.override { payload = "${kernel}/vmlinux"; };
+      bbl = crossPkgs.riscv-pk.override
+        { payload = "${kernel}/vmlinux"; };
       qemu = pkgs.qemu-riscv;
+      base-profile = pkgs.buildEnv
+        { name = "nixos-riscv-bootstrap-base-profile";
+          paths = [ crossPkgs.nixUnstable kernel busybox ];
+        };
+      image-closure = closureInfo { rootPaths = [ base-profile ]; };
+      base-image =
+        pkgs.vmTools.runInLinuxVM (pkgs.runCommand "riscv-base-nix-image"
+          { nativeBuildInputs = (map (p: pkgs.${p})
+              [ "btrfs-progs" "gptfdisk" "nixUnstable" "utillinux" ]);
+            preVM = ''
+              diskImage=$name.qcow2
+
+              imageSize=$(expr $(cat ${image-closure}/total-nar-size) + 200000000)
+              ${qemu}/bin/qemu-img create -f qcow2 $diskImage $imageSize
+            '';
+            postVM = ''
+              mkdir -p $out
+              mv $diskImage $out
+            '';
+          }
+          ''
+            sgdisk --new 1 --change-name=1:"$name" /dev/vda
+            mkfs.btrfs --label nix /dev/vda1
+            mkdir -p /mnt/nix
+            mount /dev/vda1 /mnt/nix
+            nix-store --load-db <${image-closure}/registration
+            nix-store -r ${base-profile} --option store /mnt \
+              --option substituters "/?trusted=true"
+            umount /mnt/nix
+          '');
     self =
-      {
+      { make-image = pkgs.writeShellScriptBin "make-image"
+          ''
+            declare -r file="''${1-nix-store-image.qcow2}"
+            declare -r size="''${2-${imageSize}}"
+            exec -a qemu-img ${qemu}/bin/qemu-img create -f qcow2 \
+              -b ${base-image}/riscv-base-nix-image.qcow2 "$file" \
+              $size
+          '';
         run-vm = pkgs.writeShellScriptBin "run-vm"
           ''
-            declare -r mem="''${QEMU_MEMORY_SIZE:2G}"
-            declare -r cmd="''${QEMU_KERNEL_CMDLINE:console=ttyS0}"
+            set -euo pipefail
+            declare -r image="''${NIX_STORE_IMAGE-nix-store-image.qcow2}"
+            if [ ! -f "$image" ]; then
+              echo "No nix store image, creating one sized ${imageSize}...">&2
+              ${self.make-image}/bin/make-image "$image" ${imageSize}
+            fi
+            declare -r mem="''${QEMU_MEMORY_SIZE-2G}"
+            declare -r cmd="console=ttyS0 profile=${base-profile}"
+            if [ -z ''${QEMU_EXTRA_KERNEL_CMDLINE+x} ]; then
+              declare -r full_cmd="$cmd"
+            else
+              declare -r full_cmd="$cmd $QEMU_EXTRA_KERNEL_CMDLINE"
+            fi
             exec -a qemu-system-riscv64 \
               ${qemu}/bin/qemu-system-riscv64 -nographic \
-              -machine virt -m "$mem" -append "$cmd" \
+              -machine virt -m "$mem" -append "$full_cmd" \
               -kernel ${bbl}/bin/bbl \
-              -initrd ${initrd}/initrd "$@"
+              -initrd ${initrd}/initrd -device virtio-scsi-device \
+              -device scsi-hd,drive=hd0 \
+              -drive file="$image",id=hd0 "$@"
           '';
       };
 in self
